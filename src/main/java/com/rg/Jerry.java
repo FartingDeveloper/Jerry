cdf7f9b6f5680c7c40c96aaf8788b00b8f42dd97
ruslan <r.gaflanov@amazinghiring.com>
2019-01-03 12:02:47 +0300

    Refactoring

--- src/main/java/com/rg/Jerry.java
+++ src/main/java/com/rg/Jerry.java
@@ -19,6 +19,7 @@ import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.URISyntaxException;
 import java.util.*;
+import java.util.concurrent.*;
 
 public class Jerry {
 
@@ -27,11 +28,14 @@ public class Jerry {
     private HttpServer server;
 
     public Jerry(Properties properties) throws IOException {
-        String rootPath = getRootPath();
-        ContextLoader contextLoader = new XmlContextLoader();
-        Map<String, JerryServletContext> contexts = contextLoader.load(rootPath);
+        Map<String, JerryServletContext> contexts = loadServletContexts();
+        Map<String, RequestHandler> requestHandlers = createRequestHandlers(contexts);
 
-        this.server = new HttpServer(properties, contexts);
+        ServerSocket serverSocket = createServerSocket(properties);
+
+        ExecutorService threadPool = createThreadPool(properties);
+
+        this.server = new HttpServer(serverSocket, threadPool, requestHandlers);
     }
 
     public void start() {
@@ -48,6 +52,79 @@ public class Jerry {
             throw new RuntimeException("Root path can't be found.");
         }
         return jar.getParent();
+    }
+
+    private Map<String, JerryServletContext> loadServletContexts() {
+        String rootPath = getRootPath();
+        ContextLoader contextLoader = new XmlContextLoader();
+        return contextLoader.load(rootPath);
+    }
+
+    private Map<String, RequestHandler> createRequestHandlers(Map<String, JerryServletContext> contexts) {
+        Map<String, RequestHandler> handlers = new LinkedHashMap<>();
+
+        for (String contextName : contexts.keySet()) {
+
+            JerryServletContext servletContext = contexts.get(contextName);
+
+            try {
+                servletContext.init();
+            } catch (ClassNotFoundException e) {
+                LOG.error("Class is not found error: " + contextName, e);
+                throw new IllegalStateException(e.getCause());
+            } catch (InstantiationException e) {
+                LOG.error("Context creation error: " + contextName, e);
+                throw new IllegalStateException(e.getCause());
+            } catch (ServletException e) {
+                LOG.error("Context creation error: " + contextName, e);
+                throw new IllegalStateException(e.getCause());
+            } catch (IllegalAccessException e) {
+                LOG.error("Context creation error: " + contextName, e);
+                throw new IllegalStateException(e.getCause());
+            }
+
+            Set<String> servletNames = servletContext.getServletRegistrations().keySet();
+            for (final String servletName : servletNames) {
+                JerryServletRegistration servletRegistration = (JerryServletRegistration) servletContext.getServletRegistration(servletName);
+                for (final String url : servletRegistration.getMappings()) {
+
+                    List<Filter> filters = new LinkedList<>();
+                    Map<String, JerryFilterRegistration> filterRegistrations = (Map<String, JerryFilterRegistration>) servletContext.getFilterRegistrations();
+                    for (JerryFilterRegistration filterRegistration : filterRegistrations.values()) {
+                        for (String mapping : filterRegistration.getUrlPatternMappings()) {
+                            int index = mapping.lastIndexOf("*");
+                            if (index != -1) {
+                                mapping = mapping.substring(0, index);
+                            }
+
+                            if (url.contains(mapping)) {
+                                filters.add(filterRegistration.getFilter());
+                            }
+                        }
+                    }
+
+                    RequestHandler handler = new HttpRequestHandler(servletContext, servletRegistration.getServlet(), filters);
+                    handlers.put(HTTP.URL_SEPARATOR + contextName + url, handler);
+                }
+            }
+        }
+        return handlers;
+    }
+
+    private ServerSocket createServerSocket(Properties properties) throws IOException {
+        int port = Integer.parseInt(properties.getProperty("port"));
+        return new ServerSocket(port);
+    }
+
+    private ExecutorService createThreadPool(Properties properties) {
+        int capacity = Integer.parseInt(properties.getProperty("threadPool.capacity"));
+        int corePoolSize = Integer.parseInt(properties.getProperty("threadPool.corePoolSize"));
+        int maximumPoolSize = Integer.parseInt(properties.getProperty("threadPool.maximumPoolSize"));
+        long keepAliveTime = Integer.parseInt(properties.getProperty("queue.keepAliveTime"));
+
+        BlockingQueue<Runnable> blockingQueue = new ArrayBlockingQueue<>(capacity);
+
+        return new ThreadPoolExecutor(corePoolSize, maximumPoolSize, keepAliveTime, TimeUnit.MINUTES, blockingQueue);
     }
 
     public static void main(String[] args) throws IOException {
--- src/main/java/com/rg/http/HttpServer.java
+++ src/main/java/com/rg/http/HttpServer.java
@@ -1,23 +1,16 @@
 package com.rg.http;
 
-import com.rg.http.core.HTTP;
 import com.rg.http.io.HttpRequest;
 import com.rg.http.io.HttpResponse;
-import com.rg.servlet.context.JerryServletContext;
-import com.rg.servlet.registration.JerryFilterRegistration;
-import com.rg.servlet.registration.JerryServletRegistration;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import javax.servlet.Filter;
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
-
-import java.util.*;
-import java.util.concurrent.*;
+import java.util.Map;
+import java.util.concurrent.ExecutorService;
 
 public class HttpServer extends Thread {
 
@@ -27,10 +20,10 @@ public class HttpServer extends Thread {
     private ExecutorService threadPool;
     private Map<String, RequestHandler> services;
 
-    public HttpServer(Properties properties, Map<String, JerryServletContext> contexts) throws IOException {
-        this.serverSocket = createServerSocket(properties);
-        this.threadPool = createThreadPool(properties);
-        this.services = createRequestHandlers(contexts);
+    public HttpServer(ServerSocket serverSocket, ExecutorService executorService, Map<String, RequestHandler> services) throws IOException {
+        this.serverSocket = serverSocket;
+        this.threadPool = executorService;
+        this.services = services;
     }
 
     @Override
@@ -64,72 +57,5 @@ public class HttpServer extends Thread {
                 LOG.error("Can't create response!", e);
             }
         }
-    }
-
-    private ServerSocket createServerSocket(Properties properties) throws IOException {
-        int port = Integer.parseInt(properties.getProperty("port"));
-        return new ServerSocket(port);
-    }
-
-    private ExecutorService createThreadPool(Properties properties) {
-        int capacity = Integer.parseInt(properties.getProperty("threadPool.capacity"));
-        int corePoolSize = Integer.parseInt(properties.getProperty("threadPool.corePoolSize"));
-        int maximumPoolSize = Integer.parseInt(properties.getProperty("threadPool.maximumPoolSize"));
-        long keepAliveTime = Integer.parseInt(properties.getProperty("queue.keepAliveTime"));
-
-        BlockingQueue<Runnable> blockingQueue = new ArrayBlockingQueue<>(capacity);
-
-        return new ThreadPoolExecutor(corePoolSize, maximumPoolSize, keepAliveTime, TimeUnit.MINUTES, blockingQueue);
-    }
-
-    private Map<String, RequestHandler> createRequestHandlers(Map<String, JerryServletContext> contexts) {
-        Map<String, RequestHandler> handlers = new LinkedHashMap<>();
-
-        for (String contextName : contexts.keySet()) {
-
-            JerryServletContext servletContext = contexts.get(contextName);
-
-            try {
-                servletContext.init();
-            } catch (ClassNotFoundException e) {
-                LOG.error("Class is not found error: " + contextName, e);
-                throw new IllegalStateException(e.getCause());
-            } catch (InstantiationException e) {
-                LOG.error("Context creation error: " + contextName, e);
-                throw new IllegalStateException(e.getCause());
-            } catch (ServletException e) {
-                LOG.error("Context creation error: " + contextName, e);
-                throw new IllegalStateException(e.getCause());
-            } catch (IllegalAccessException e) {
-                LOG.error("Context creation error: " + contextName, e);
-                throw new IllegalStateException(e.getCause());
-            }
-
-            Set<String> servletNames = servletContext.getServletRegistrations().keySet();
-            for (final String servletName : servletNames) {
-                JerryServletRegistration servletRegistration = (JerryServletRegistration) servletContext.getServletRegistration(servletName);
-                for (final String url : servletRegistration.getMappings()) {
-
-                    List<Filter> filters = new LinkedList<>();
-                    Map<String, JerryFilterRegistration> filterRegistrations = (Map<String, JerryFilterRegistration>) servletContext.getFilterRegistrations();
-                    for (JerryFilterRegistration filterRegistration : filterRegistrations.values()) {
-                        for (String mapping : filterRegistration.getUrlPatternMappings()) {
-                            int index = mapping.lastIndexOf("*");
-                            if (index != -1) {
-                                mapping = mapping.substring(0, index);
-                            }
-
-                            if (url.contains(mapping)) {
-                                filters.add(filterRegistration.getFilter());
-                            }
-                        }
-                    }
-
-                    RequestHandler handler = new HttpRequestHandler(servletContext, servletRegistration.getServlet(), filters);
-                    handlers.put(HTTP.URL_SEPARATOR + contextName + url, handler);
-                }
-            }
-        }
-        return handlers;
     }
 }
